09625fb2d2ce387446b63575468d286167818d71
KunalBhamoriya <135410677+KunalBhamoriya@users.noreply.github.com>
2025-11-27 18:03:13 +0530

    tailwind-react-project

--- CodeFlow/src/components/Footer.jsx
+++ CodeFlow/src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='flex justify-center w-full h-16 bg-slate-900/50 backdrop-blur-xl'>
+        <div className='text-center align-middle'>
+          <p className='mt-5'>Copyright &copy; 2025</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
